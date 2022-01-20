b211a3287b03498da92c6336818829cdc4c95493
Abdullah-MotiWala <motiwalaabdullah100@gmail.com>
2022-01-21 00:35:49 +0500

    resolve error in index server file and pkg.json

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+const express = require("express");
+const app = express();
 if (process.env.NODE_ENV !== "production") {
     app.use(express.static("i_note_book/build"))
 }
 const dotEnv = require("dotenv").config({ path: "./config.env" });
 const connectingMongoose = require("./db");
-const express = require("express");
 const cors = require("cors");
 
 // const { Schema } = mongoose;
 
 connectingMongoose();
 
-const app = express();
 const port = process.env.PORT || 5002;
 app.use(express.json());
 app.use(cors());
